5760ff1cc732f3bf0192f7eddeb7d4bbbbc66738
harshit Goyal <harshitgo0409@gmail.com>
2023-12-13 17:41:53 +0530

    changes done

--- index.js
+++ index.js
@@ -7,22 +7,19 @@ const server = express();
 const path = require('path');
 const productRouter = require('./routes/product')
 const userRouter = require('./routes/user')
-console.log('env',process.env.DB_PASSWORD)
+//console.log('env',process.env.DB_PASSWORD)
 
 //db connection
-main().catch(err => console.log(err));
-
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
-  console.log('database connected')
+  try {
+    await mongoose.connect('mongodb+srv://harshitgoyal1234567890:IrkgmlRBmiLQtPVw@cluster0.ghcazld.mongodb.net/ecommercetest?retryWrites=true&w=majority');
+    console.log('Database connected');
+  } catch (error) {
+    console.error('Error connecting to the database:', error.message);
+  }
 }
+main();
 //Schema
-
-
-
-
-
-
 
 
 //bodyParser
@@ -33,12 +30,12 @@ server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
 server.use('/products',productRouter.router);
 server.use('/users',userRouter.router);
 server.use('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'build','index.html'))
+  res.sendFile(path.resolve(__dirname,'build','index.html'))
 })
 
 
 
 
-server.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => { 
   console.log('server started');
 });
